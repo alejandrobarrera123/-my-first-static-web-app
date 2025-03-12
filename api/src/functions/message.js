320100f016a1a1d957d9d788145fb2ea8482e353
Diego Barrera <alejandrobarrera123@gmail.com>
2025-03-11 22:08:39 -0500

    add

--- api/src/functions/message.js
+++ api/src/functions/message.js
@@ -1,13 +1,9 @@
 const { app } = require('@azure/functions');
 
 app.http('message', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
-
-        const name = request.query.get('name') || await request.text() || 'world';
-
-        return { body: `Hello, ${name}!` };
-    }
+  methods: ['GET', 'POST'],
+  authLevel: 'anonymous',
+  handler: async (request, context) => {
+    return { body: `Hello, from the API!` };
+  },
 });
